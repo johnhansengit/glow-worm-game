03b07513ac59a9ecb285f3d1fa8eeec13fdfa3d9
John Hansen <jhb.rgn@gmail.com>
2023-11-28 16:59:45 -0500

    Start to dev landing page

--- setup.js
+++ setup.js
@@ -4,6 +4,15 @@
 
 /*------------------------- CACHED ELEMENTS -------------------------*/
 
+//Hamburger Nav Menu
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navLinks = document.getElementById('hamburger-nav-links');
+
 /*------------------------- EVENT LISTENERS -------------------------*/
+
+// Hamburger Nav Menu Functionality
+hamburgerBtn.addEventListener('click', function() {
+    navLinks.style.display = (navLinks.style.display == 'none') ? 'flex' : 'none';
+});
 
 /*------------------------- INIT FUNCTIONS -------------------------*/
