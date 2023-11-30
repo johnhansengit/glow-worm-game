757a352c3f537f5964aaf26b76e685cfcc0a6ede
John Hansen <jhb.rgn@gmail.com>
2023-11-30 13:41:31 -0500

    Move hamburger/go-to-game code to landing.js

--- setup.js
+++ setup.js
@@ -56,11 +56,6 @@ let points;
 
 /*------------------------- CACHED ELEMENTS -------------------------*/
 
-// Hamburger Nav Menu
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const navLinks = document.getElementById('hamburger-nav-links');
-const goToGameBtn = document.getElementById('go-to-game');
-
 // Stats
 const levelEl = document.getElementById('level');
 const highScoreEl = document.getElementById('high-score-val');
@@ -88,24 +83,6 @@ const leftArrow = document.getElementById('arrow-left');
 
 
 /*------------------------- EVENT LISTENERS -------------------------*/
-
-// Hamburger Nav Menu Functionality
-hamburgerBtn.addEventListener('click', function () {
-    if (navLinks.style.opacity == '0' || navLinks.style.opacity == '') {
-        navLinks.style.opacity = '1';
-        navLinks.style.visibility = 'visible';
-    } else {
-        navLinks.style.opacity = '0';
-        setTimeout(() => { navLinks.style.visibility = 'hidden'; }, 500);
-    }
-});
-
-// Go To Game Button
-if (goToGameBtn) {
-    goToGameBtn.addEventListener('click', function () {
-        window.location.href = 'game.html';
-    });
-}
 
 // Start/Restart/Quit Game Buttons
 if (startBtn) {
